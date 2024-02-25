8d88a2ae347ae08e27855964775f942afe7fd77e
Uriel <Uriel>
2024-02-25 12:23:10 -0700

    Baraja de cartas ordenada para jugar

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from stack import Stack
 from os import system
 from random import randint
 
+def makeStickDeck(color, icon, stick):
+    card = Card(color, icon, stick)
+    cards = makeSticks(card)
+    setValueStick(cards)
+    
+    return cards
+
 def makeSticks(card):
     return [Card(card.color, card.icon, card.stick) for _ in range(13)]
 
@@ -27,8 +34,19 @@ def printDeck(deckOfCards):
         if i == 12 or i == 25 or i == 38:
             print()
     print()
-        
-            
+
+def pushSeveral(deck, num):
+    stack = Stack()
+    for i in range(num):
+        stack.push(deck[i])
+    deleteSomeItems(deck, num)    
+    
+    return stack
+
+def deleteSomeItems(deck, num):
+    for i in range(num-1, -1, -1):
+        deck.pop(i)
+                 
 if __name__ == "__main__":
     
     print("//Solitario//")
@@ -37,37 +55,60 @@ if __name__ == "__main__":
     endGame = False
     while not endGame:
         
-        # Definir listas de cada palo, y su valor en cada posicion
-        heard = Card("red", "<3", "hearts")
-        heards = makeSticks(heard)
-        setValueStick(heards)
-        
-        diamond = Card("red", "<>", "diamonds")
-        diamonds = makeSticks(diamond)
-        setValueStick(diamonds)
-        
-        trevol = Card("black", "-3", "clubs")
-        trevols = makeSticks(trevol)
-        setValueStick(trevols)
-        
-        pica = Card("black", "->", "spades")
-        picas = makeSticks(pica)
-        setValueStick(picas)
-        
-        # Hacer baraja de cartas revueltas
+        # Crear palos
+        heards = makeStickDeck("red", "<3", "hearts")    
+        diamonds = makeStickDeck("red", "<>", "diamonds")
+        trevols = makeStickDeck("black", "-3", "clubs")    
+        picas = makeStickDeck("black", "->", "spades")
+    
+        # Hacer baraja de solitario de cartas revueltas
         deckOfCards = heards + diamonds + trevols + picas
         deckOfCards = shuffleCards(deckOfCards)
         
+        # Imprimir baraja de cartas (PRUEBAS)
         printDeck(deckOfCards)
         input("para")
         
-
-
-
+        # Crear pila1
+        stack1 = pushSeveral(deckOfCards, int(1))
+        stack1.printCardStack()
         
+        # Crear pila2
+        stack2 = pushSeveral(deckOfCards, int(2))
+        stack2.printCardStack()
+        
+        # Crear pila3
+        stack3 = pushSeveral(deckOfCards, int(3))
+        stack3.printCardStack()
+        
+        # Crear pila4
+        stack4 = pushSeveral(deckOfCards, int(4))
+        stack4.printCardStack()
+        
+        # Crear pila5
+        stack5 = pushSeveral(deckOfCards, int(5))
+        stack5.printCardStack()
+        
+        # Crear pila6
+        stack6 = pushSeveral(deckOfCards, int(6))
+        stack6.printCardStack()
+        
+        # Crear pila7
+        stack7 = pushSeveral(deckOfCards, int(7))
+        stack7.printCardStack()
+        
+        print("_______________")
+        printDeck(deckOfCards)
+        print("len: ", len(deckOfCards))
+        
+        # Crear pila 0 (cartas sobrantes)
+        print("PILA0:")
+        stack0 = pushSeveral(deckOfCards, int(24))
+        stack0.printCardStack()
+        print()
+        
+        input("stop")
+    
+    
 
-
-        
-        
-        
     
--- stack.py
+++ stack.py
@@ -1,3 +1,5 @@
+from card import Card
+
 class Stack:
     MAX_SIZE = 100
 
@@ -5,33 +7,40 @@ class Stack:
         self.elements = [0] * self.MAX_SIZE
         self.top = -1
 
-    def is_empty(self):
+    def isEmpty(self):
         return self.top == -1
 
-    def is_full(self):
+    def isFull(self):
         return self.top == self.MAX_SIZE - 1
 
     def push(self, element):
-        if not self.is_full():
+        if not self.isFull():
             self.top += 1
             self.elements[self.top] = element
         else:
             print("Error: La pila está llena")
 
     def pop(self):
-        if not self.is_empty():
+        if not self.isEmpty():
             self.top -= 1
         else:
             print("Error: La pila está vacía")
 
-    def get_top(self):
-        if not self.is_empty():
+    def getTop(self):
+        if not self.isEmpty():
             return self.elements[self.top]
         else:
             print("Error: La pila está vacía")
 
-    def print_stack(self):
+    def printStack(self):
         print("Contenido de la pila:", end=" ")
         for i in range(self.top + 1):
             print(self.elements[i], end=", ")
         print()
+    
+    def printCardStack(self):
+        print("Pila: ", end=" ")
+        for i in range(self.top + 1):
+            print(self.elements[i].getValue(), end=", ")
+        print()
+    
